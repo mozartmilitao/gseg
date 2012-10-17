de607780432e833bc85b54ebad90c26cc3d4ed66
Daniel Cunha <danielsoro@gmail.com>
2012-10-16 21:17:22 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gseg::Application.config.secret_token = 'f3b7097f76f75473d524834d3f4b00f89b3c2541d770e33b0b00c8859762965f1c57f5ab999e4fe7ac50cea38f9152d025c2508e7201042fd4e04ca98c1ff7b0'
