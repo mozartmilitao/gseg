996da2b2e669efb1b0ba4e6ceff40e006c0666c8
Daniel Cunha <danielsoro@gmail.com>
2012-10-16 22:28:54 -0300

    Adição dos testes iniciais para modelo do tipo_seguros

--- app/models/tipo_seguro.rb
+++ app/models/tipo_seguro.rb
@@ -1,3 +1,4 @@
 class TipoSeguro < ActiveRecord::Base
   attr_accessible :descricao
+  validates :descricao, presence: true, uniqueness: true
 end
--- test/unit/tipo_seguro_test.rb
+++ test/unit/tipo_seguro_test.rb
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
-class TipoSeguroTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class TipoSeguroTest < ActiveSupport::TestCase  
+  test "não deve aceitar tipo_seguro sem descricao" do
+    tipo_seguro = TipoSeguro.new
+    assert !tipo_seguro.save
+  end
+  
+  test "não deve aceitar dois tipo_seguro com mesma descricao" do
+    tipo_seguro1 = TipoSeguro.new descricao: "T"
+    tipo_seguro2 = TipoSeguro.new descricao: "T"
+    
+    assert tipo_seguro1.save
+    assert !tipo_seguro2.save
+  end
 end
