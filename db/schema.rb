59b477e72e3d56b4bc779afec52c6ee0ce43ddce
Daniel Cunha <danielsoro@gmail.com>
2012-10-17 05:59:05 -0300

    Alteração rake db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121017002306) do
+ActiveRecord::Schema.define(:version => 20121017084905) do
+
+  create_table "tipo_documentos", :force => true do |t|
+    t.string   "descricao"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "tipo_seguros", :force => true do |t|
     t.string   "descricao"
